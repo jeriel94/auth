2b39628932b7aee462135c79890afe8087696271
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-05-18 02:36:19 +0000

    new login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,23 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email" => params["emails"]})
     # 2. if the user exists -> check if they know their password
+    if @user != nil
     # 3. if they know their password -> login is successful
+    if @user["password"] ==  params["password"]
     # 4. if the email does not exist or they do not know their password -> login fails
+    flash["notice"] = "Welcome."
+    redirect_to "/companies"
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
+  else
     flash["notice"] = "Nope."
     redirect_to "/sessions/new"
   end
+end
 
   def destroy
     # logout the user
